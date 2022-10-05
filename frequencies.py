c7248a81cdeb90c3a8ca43c72d36ec06fc903ac8
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-05 17:49:02 +0100

    Create a map which shows each key's frequencies

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,19 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+# Return a dictionary mapping items to their frequencies
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    # Iterate through all items
+    for item in items:
+        # convert item into strings
+        itemStr = str(item)
+        # Increment count of each item by 1
+        if itemStr in frequencies:
+            frequencies[itemStr] += 1
+        # If there was no item, set the count as 1
+        else:
+            frequencies[itemStr] = 1
+    # Return the dictionary
     return frequencies
