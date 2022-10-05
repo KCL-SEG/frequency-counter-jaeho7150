0385f0d0b768a08095a6b46c341ef23d87aa97a9
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-05 18:56:51 +0100

    Return a dictionary in which each key has its frequencies as a value

--- frequencies.py
+++ frequencies.py
@@ -1,19 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-# Return a dictionary mapping items to their frequencies
 def frequencies(items):
     frequencies = {}
 
-    # Iterate through all items
     for item in items:
-        # convert item into strings
-        itemStr = str(items)
-        # Increment count of each item by 1
-        if itemStr in frequencies:
-            frequencies[itemStr] += 1
-        # If there was no item, set the count as 1
+        if item in frequencies:
+            frequencies[str(item)] += 1
         else:
-            frequencies[itemStr] = 1
-    # Return the dictionary
+            frequencies[str(item)] = 1
     return frequencies
